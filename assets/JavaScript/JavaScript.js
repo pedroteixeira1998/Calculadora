2f033da842086b396b4e3b258be04f5f7be7692c
pedroteixeira1998 <pteixeira98@live.com>
2023-03-14 18:39:06 -0300

    Controlar a quantidade de pontos na operação

--- assets/JavaScript/JavaScript.js
+++ assets/JavaScript/JavaScript.js
@@ -1,5 +1,12 @@
 function insert(n){
     var num = document.querySelector("#tela").innerHTML;
+    var lastChar = num[num.length - 1];
+    if( n === "." && lastChar === "."){
+        return;}
+    if( n === "." && num.indexOf(".") !== -1){
+    
+        return;}
+
     document.querySelector("#tela").innerHTML = (num + n);
 }
 
